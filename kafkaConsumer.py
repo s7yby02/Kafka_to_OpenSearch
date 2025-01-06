81b0f614ac7a2058896204a48dd463b08e6eeb5c
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2025-01-06 01:53:46 +0100

    adding opensearch indexation

--- kafkaConsumer.py
+++ kafkaConsumer.py
@@ -1,5 +1,6 @@
 from kafka import KafkaConsumer
 import json
+from opensearchpy import OpenSearch
 
 # Kafka Consumer setup
 consumer = KafkaConsumer(
@@ -10,12 +11,30 @@ consumer = KafkaConsumer(
     value_deserializer=lambda v: json.loads(v.decode('utf-8'))
 )
 
+# OpenSearch Client setup
+os_client = OpenSearch(
+    hosts=[{'host': 'localhost', 'port': 9200}],
+    http_auth=('admin', 'sdlSDFqkjgsd6842sdgkjsqdg+dfs:'),
+    use_ssl=True,
+    verify_certs=False
+)
+
+index_name = "wikimedia"
+# Ensure the index exists
+if not os_client.indices.exists(index=index_name):
+    os_client.indices.create(index=index_name)
+
 try:
     print("Kafka Consumer is running...")
     for message in consumer:
         # Process the received message
         data = message.value
-        print(f"Received message: User: {data['user']}, Title: {data['title']}")
+        # Index the message into OpenSearch
+        try:
+            os_client.index(index=index_name, body=data)
+            print(f"Received message: User: {data['user']}, Title: {data['title']}")
+        except:
+            pass
 except KeyboardInterrupt:
     print("Consumer interrupted by user.")
 finally:
